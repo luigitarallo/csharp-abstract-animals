c1ba3a71961e6b5cdf290d08dc2a50ec886bdf40
luigitarallo <tarallo.gigi@gmail.com>
2024-04-24 15:43:53 +0200

    add SuperClass Animale e Subclasses, add List animali with foreach

---
+++ csharp-abstract-animals/Animale.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_abstract_animals
+{
+    public abstract class Animale
+    {
+        public void Dormi()
+        {
+            Console.WriteLine("Zzzzzz");
+        }
+
+        public abstract void Verso();
+        public abstract void Mangia();
+        public abstract void CheAnimale();
+    }
+
+    public class Cane : Animale
+    {
+        public override void Verso()
+        {
+            Console.WriteLine("Faccio bau bau!");
+        }
+
+        public override void Mangia()
+        {
+            Console.WriteLine("Mangio crocchette e carne");
+        }
+
+        public override void CheAnimale()
+        {
+            Console.WriteLine("Sono un cane!");
+        }
+    }
+
+    public class Passerotto : Animale
+    {
+        public override void Verso()
+        {
+            Console.WriteLine("Faccio cip cip!");
+        }
+
+        public override void Mangia()
+        {
+            Console.WriteLine("Mangio insetti e frutta");
+        }
+
+        public override void CheAnimale()
+        {
+            Console.WriteLine("Sono un passerotto!");
+        }
+    }
+
+    public class Aquila : Animale
+    {
+        public override void Verso()
+        {
+            Console.WriteLine("Faccio screech!");
+        }
+
+        public override void Mangia()
+        {
+            Console.WriteLine("Mangio piccoli mammiferi");
+        }
+
+        public override void CheAnimale()
+        {
+            Console.WriteLine("Sono un'aquila!");
+        }
+    }
+
+    public class Delfino : Animale
+    {
+        public override void Verso()
+        {
+            Console.WriteLine("Faccio click click!");
+        }
+
+        public override void Mangia()
+        {
+            Console.WriteLine("Mangio pesci");
+        }
+
+        public override void CheAnimale()
+        {
+            Console.WriteLine("Sono un delfino!");
+        }
+    }
+}
--- csharp-abstract-animals/Program.cs
+++ csharp-abstract-animals/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<Animale> animali = new List<Animale>();
+
+            animali.Add(new Cane());
+            animali.Add(new Passerotto());
+            animali.Add(new Aquila());
+            animali.Add(new Delfino());
+
+            foreach (Animale animale in animali)
+            {
+                animale.Dormi();
+                animale.Verso();
+                animale.Mangia();
+                animale.CheAnimale();
+            }
         }
     }
 }
